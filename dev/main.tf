78816e6e6b1b0f9a15c51bd87961c05f446adf5f
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-12-28 23:06:29 +0000

    added other vpc resources

--- dev/main.tf
+++ dev/main.tf
@@ -6,3 +6,17 @@ resource "aws_vpc" "wp-vpc" {
         Name = "wp-vpc"
     }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "wp-subnet" {
+  vpc_id = aws_vpc.wp-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_internet_gateway" "wp-igw" {
+  vpc_id = aws_vpc.wp-vpc.id
+}
