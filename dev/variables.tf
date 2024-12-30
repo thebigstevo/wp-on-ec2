211bde0e3325f210d9ac24b4110d6ab5f26d029e
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-12-30 11:43:32 +0000

    introduced variables

---
+++ dev/variables.tf
@@ -0,0 +1,35 @@
+variable "enable_dns_hostnames" {
+  default = true
+  
+}
+variable "enable_dns_support" {
+  default = true
+  
+}
+  
+variable "vpc_cidr_block" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "subnet_cidr_block" {
+    type = string
+    default = "10.0.1.0/24"
+  
+}
+
+variable "ami" {
+  default = "ami-0e9085e60087ce171" # Ubuntu 24.04 AMI for eu-west-1
+  
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+  
+}
+
+variable "associate_public_ip_address" {
+  default = true
+  
+}
