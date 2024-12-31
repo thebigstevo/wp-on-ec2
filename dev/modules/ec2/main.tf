a707196c683f6a831150528070da9aec04597137
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-12-31 11:10:07 +0000

    updated sg parameter

--- dev/modules/ec2/main.tf
+++ dev/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "wordpress_ec2" {
   instance_type = var.instance_type
 
   subnet_id                   = var.public_subnet_id
-  vpc_security_group_ids            = var.vpc_security_group_ids
+  security_groups       = var.vpc_security_group_ids
   associate_public_ip_address = var.associate_public_ip_address
 
   iam_instance_profile = var.ssm_instance_profile
